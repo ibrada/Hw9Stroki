e3604dc79dfbb985724310becf0503716e8c2e75
ibrada <radikibragimov@mail.ru>
2025-02-18 19:35:47 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        {
+            String firstName = "Ivan";
+            String middleName = "Ivanovich";
+            String lastName = "Ivanov";
+            String fullName = lastName + " " + firstName + " " + middleName;
+            System.out.println("Ф.И.О. сотрудника - " + fullName);
+        }
+        {
+            String fullName = "Ivanov Ivan Ivanovich";
+            System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        }
+        {
+            String fullName = "Иванов Семён Семёнович";
+            System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replaceAll("ё", "е"));
+        }
     }
 }
